9b7a3d2e7062fe6c04e1913b72350e7e762dffb5
taitamfc <taitam8590@gmail.com>
2023-04-21 10:26:45 +0700

    config

--- config.php
+++ config.php
@@ -2,8 +2,11 @@
 // Cấu hình ứng dụng
 
 // Đường dẫn
-define('PUBLIC_URL','');
+// /CODEGYM/template-manage-library
+define('ROOT_URL',dirname($_SERVER['PHP_SELF']));
+// /CODEGYM/template-manage-library/public
+define('PUBLIC_URL',ROOT_URL.'/public');
+// C:\laragon\www\CODEGYM\template-manage-library
 define('ROOT_DIR',dirname(__FILE__));
 
-echo ROOT_DIR;
 // Cấu hình CSDL
